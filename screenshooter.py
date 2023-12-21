bef25bb61a90025c1e86b8c3f5de4f527cd76219
Roger Garcia <96830104+Rogergarciatsa@users.noreply.github.com>
2023-12-21 13:02:36 +0000

    Update screenshooter.py

--- screenshooter.py
+++ screenshooter.py
@@ -2,32 +2,42 @@ import asyncio
 import os
 from pyppeteer import launch
 from PIL import Image
-import webptools
 
 async def capture_screenshot(html_file_path, screenshot_path):
     browser = await launch(headless=True)
     page = await browser.newPage()
     
-    # Load the HTML file
-    await page.goto(f'file://{os.path.abspath(html_file_path)}', waitUntil='domcontentloaded')
+    try:
+        # Load the HTML file
+        await page.goto(f'file://{os.path.abspath(html_file_path)}', waitUntil='domcontentloaded')
 
-    # Wait for a fixed duration (e.g., 5 seconds)
-    await asyncio.sleep(5)
+        # Wait for a fixed duration (e.g., 5 seconds)
+        await asyncio.sleep(5)
 
-    # Take a screenshot of the whole page
-    await page.screenshot({'path': screenshot_path, 'fullPage': True})
-
-    await browser.close()
+        # Take a screenshot of the whole page
+        await page.screenshot({'path': screenshot_path, 'fullPage': True})
+    except Exception as e:
+        print(f"An error occurred while taking a screenshot: {e}")
+    finally:
+        await browser.close()
 
 def crop_to_1200px(input_path, output_path):
-    image = Image.open(input_path)
-    width, height = image.size
-    top_crop = max(0, height - 1200)  # Crop from the top, leaving the bottom
-    cropped_image = image.crop((0, top_crop, width, height))
-    cropped_image.save(output_path)
+    try:
+        image = Image.open(input_path)
+        width, height = image.size
+        bottom_crop = max(0, height - 1200)  # Crop from the bottom, leaving the top
+        cropped_image = image.crop((0, 0, width, height - bottom_crop))
+        cropped_image.save(output_path)
+    except Exception as e:
+        print(f"An error occurred while cropping the image: {e}")
 
-def convert_to_webp(input_path, output_path):
-    webptools.dwebp(input_path, output_path, '-o', '-quiet')
+
+def save_as_webp(input_path, output_path):
+    try:
+        image = Image.open(input_path)
+        image.save(output_path, "WEBP", quality=95)
+    except Exception as e:
+        print(f"An error occurred while saving as WebP: {e}")
 
 # Set the path to the extracted folder
 extracted_folder = 'extracted'
@@ -52,16 +62,7 @@ try:
                 cropped_screenshot_path = os.path.join(folder_path, cropped_screenshot_file)
                 crop_to_1200px(screenshot_path, cropped_screenshot_path)
 
-                webp_screenshot_file = f"{subject_name}.webp"
-                webp_screenshot_path = os.path.join(folder_path, webp_screenshot_file)
-                convert_to_webp(screenshot_path, webp_screenshot_path)
-
-                webp_cropped_screenshot_file = f"{subject_name}_cropped.webp"
-                webp_cropped_screenshot_path = os.path.join(folder_path, webp_cropped_screenshot_file)
-                convert_to_webp(cropped_screenshot_path, webp_cropped_screenshot_path)
-
-                print(f"Screenshots saved: {screenshot_path}, {cropped_screenshot_path}, {webp_screenshot_path}, {webp_cropped_screenshot_path}")
+                print(f"Screenshots saved: {screenshot_path}, {cropped_screenshot_path}")
 
 except Exception as e:
-    print(f"An error occurred: {e}")
-    raise  # Add this line to see the full stack trace
+    print(f"An error occurred while processing folders: {e}")
