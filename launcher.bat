6589b64c01d42a4a7e3bd133e7f11c4400657cb0
Roger Garcia <rogergarciatsa@gmail.com>
2023-12-27 02:38:05 +0100

    launcher 1st commit

---
+++ launcher.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Running IMAP email scraper from server...
+python C:\path\to\your\script1.py
+echo Scraper script 1 completed.
+echo Running Screenshooter Email...
+python C:\path\to\your\script2.py
+echo Script 2 completed.
